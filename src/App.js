02a29d22a7b050f1373c95573beed63d0324251e
Dominik Brame <idominikbrame@gmail.com>
2021-07-30 21:25:51 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react"
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://randomuser.me/api?results=25')
+            .then(res => {
+                let contacts = res.data.results
+                console.log(contacts)
+            })
+    }
+
+
+    render(){
+        return (
+            <div className="App">
+
+            </div>
+        )
+    }
 }
 
-export default App;
+    export default App;
